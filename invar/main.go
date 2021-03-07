3fb079f3fdde384da91ea3feba1a5d7f19d049b9
tomdragon_0@163.com <Chris13975>
2021-03-07 11:39:51 +0800

    feat: invar

---
+++ invar/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
+
+func main()  {
+	fmt.Println("invar")
+	cmplx2()
+	traingle()
+}
+
+func cmplx2()  {
+	c := 3 + 4i // 3^2+4^2=5^2
+	fmt.Println(cmplx.Abs(c))
+}
+
+func traingle()  {
+	var i, j int = 3, 4
+	var k int
+	k = int(math.Sqrt(float64(i*  + j*j)))
+	fmt.Println("k = ", k)
+}
